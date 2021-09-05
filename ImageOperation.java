1642e395d4d9e07206b1e6a66854a7427cddb198
Almost-Anmol <anmolg618.ag@gmail.com>
2021-09-06 01:31:20 +0530

    Complete

--- ImageOperation.java
+++ ImageOperation.java
@@ -1,37 +1,76 @@
 import java.awt.Font;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class ImageOperation {
-    public static void main(String[] args) {
 
+    public static void operation(int key) {
+        JFileChooser selectFile = new JFileChooser();
+        selectFile.showOpenDialog(null);
+        File file = selectFile.getSelectedFile();
+        // file input stream reader
+        try {
+
+            FileInputStream fin = new FileInputStream(file);
+
+            byte[] data = new byte[fin.available()];
+            fin.read(data);
+            int i = 0;
+            for (byte b : data) {
+                System.out.println(b);
+                data[i] = (byte) (b ^ key);
+                i++;
+            }
+            // file output stream writer
+            FileOutputStream fout = new FileOutputStream(file);
+            fout.write(data);
+            fout.close();
+            fin.close();
+            JOptionPane.showMessageDialog(null, "Operation Successful !");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
 
         JFrame frame = new JFrame();
         frame.setTitle("Image Operation");
-        frame.setSize(500, 500);
+        frame.setSize(400, 400);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Font font = new Font("Bali", Font.BOLD, 25);
-        //creating button
+
+        // creating text field
+        JTextField text = new JTextField(10);
+        text.setFont(font);
+
+        // creating button
         JButton button = new JButton();
         button.setText("Select Image");
-button.setFont(font);
+        button.setFont(font);
 
-// creating text field
-JTextField text = new JTextField(10);
-text.setFont(font);
+        frame.setLayout(new FlowLayout());
+        frame.add(button);
+        frame.add(text);
+        frame.setVisible(true);
 
-frame.setLayout(new FlowLayout());
-frame.add(button);
-frame.add(text);
+        button.addActionListener(e -> {
+            String input = text.getText();
+            int key = Integer.parseInt(input);
+            operation(key);
+        });
 
-frame.setVisible(true);
-
-
-        
-     System.out.println("this is test");   
+        // System.out.println("this is test");
     }
 }
